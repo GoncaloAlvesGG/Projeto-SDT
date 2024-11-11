e5ee930c4a2184844e21ee245270d05b3f58dae0
DankLord69420 <stevecraftkid@gmail.com>
2024-11-11 10:20:00 +0000

    Update Node.java

--- Projeto/src/Node.java
+++ Projeto/src/Node.java
@@ -1,12 +1,9 @@
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.rmi.RemoteException;
 
 public class Node {
 
-    private static final String MULTICAST_ADDRESS = "230.0.0.0"; // Endereço Multicast
-    private static final int PORT = 8888; // Porta Multicast
+    protected static final String MULTICAST_ADDRESS = "230.0.0.0"; // Endereço Multicast
+    protected static final int PORT = 8888; // Porta Multicast
 
     // Array com três mensagens de exemplo
     private static final String[] MESSAGES = {
@@ -16,41 +13,20 @@ public class Node {
     };
 
     private final boolean isLeader;
+    //private final Set<String> receiveAcks = new HashSet<>(); // Armazena UUIDs dos elementos que enviaram ACKs
 
-    public Node(boolean isLeader) {
+    public Node(boolean isLeader) throws RemoteException {
         this.isLeader = isLeader;
+
     }
+
 
     public void start() {
         if (isLeader) {
-            SendTransmitter transmitter = new SendTransmitter(MULTICAST_ADDRESS, PORT, MESSAGES);
-            transmitter.start();
+            System.out.println("Este nó é o líder. Inicie classe LeaderNode.");
         } else {
-            ListenTransmitter listener = new ListenTransmitter(MULTICAST_ADDRESS, PORT);
+            ListenTransmitter listener = new ListenTransmitter(MULTICAST_ADDRESS, PORT, this);
             listener.start();
         }
-    }
-
-
-    // Ouve mensagens de heartbeat se for um membro
-    private void listenForHeartbeats() {
-        Thread receiveThread = new Thread(() -> {
-            try (MulticastSocket socket = new MulticastSocket(8888)) {
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                socket.joinGroup(group); // Junta-se ao grupo multicast
-
-                byte[] buffer = new byte[256];
-                while (true) {
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                    socket.receive(packet);
-
-                    String receivedMessage = new String(packet.getData(), 0, packet.getLength());
-                    System.out.println("Mensagem recebida: " + receivedMessage);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        receiveThread.start();
     }
 }
